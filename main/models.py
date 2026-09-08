4fc3562aec4965c353ab0f0a40a0d64cce380b9e
Rois <roisul.umam@ui.ac.id>
2026-09-08 13:22:53 +0700

    feat(portfolio): add dynamic skill and project models with admin integration also changing web design to glassmorphism theme

---
+++ main/models.py
@@ -0,0 +1,45 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class Experience(models.Model):
+    EXPERIENCE_CHOICES = [
+        ('internship', 'Internship'),
+        ('research', 'Research'),
+        ('volunteer', 'Volunteer'),
+        ('part-time', 'Part-Time'),
+        ('full-time', 'Full-Time'),
+        ('freelance', 'Freelance'),
+        ('competition', 'Competition'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
+    thumbnail = models.URLField(blank=True, null=True)
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(blank=True, null=True)
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
+    
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    tags = models.CharField(max_length=200) # Disimpan sebagai: "Django, Python, CSS"
+    link = models.URLField(blank=True, null=True)
+
+    @property
+    def get_tags_list(self):
+        return [tag.strip() for tag in self.tags.split(',') if tag.strip()]
